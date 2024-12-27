f44801149ae0a699dcda34492cd9ab6340283f7a
arashmad <arashmad.dev@gmail.com>
2024-12-27 23:45:19 +0100

    plugged in /users endpoint into app

--- user_management_py/routes/api.py
+++ user_management_py/routes/api.py
@@ -4,10 +4,15 @@ Fastapi Poetry Boilerplate.
 A boilerplate for fastapi python project supported by poetry.
 """
 
+import user_management_py.routes.endpoints.home as home_route
+import user_management_py.routes.endpoints.pipelines as pipelines_route
+import user_management_py.routes.endpoints.users as users_route
 from fastapi import APIRouter
-
-from user_management_py.routes.endpoints import home
 
 router = APIRouter()
 
-router.include_router(home.router, tags=["Home"])
+router.include_router(home_route.router, tags=["Home"])
+router.include_router(users_route.router, tags=["Users"],
+                      prefix="/users")
+# router.include_router(pipelines_route.router, tags=["Pipelines"],
+#                       prefix="/pipelines")
