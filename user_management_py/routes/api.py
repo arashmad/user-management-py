1d2ea0ac8335af7564651d9c6618ebdbb477a4f4
arashmad <arashmad.dev@gmail.com>
2024-12-27 23:47:58 +0100

    unplugged /pipelines and fixed lint

--- user_management_py/routes/api.py
+++ user_management_py/routes/api.py
@@ -4,15 +4,13 @@ Fastapi Poetry Boilerplate.
 A boilerplate for fastapi python project supported by poetry.
 """
 
-import user_management_py.routes.endpoints.home as home_route
-import user_management_py.routes.endpoints.pipelines as pipelines_route
-import user_management_py.routes.endpoints.users as users_route
 from fastapi import APIRouter
+
+import user_management_py.routes.endpoints.home as home_route
+import user_management_py.routes.endpoints.users as users_route
 
 router = APIRouter()
 
 router.include_router(home_route.router, tags=["Home"])
 router.include_router(users_route.router, tags=["Users"],
                       prefix="/users")
-# router.include_router(pipelines_route.router, tags=["Pipelines"],
-#                       prefix="/pipelines")
