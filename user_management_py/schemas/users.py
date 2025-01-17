78dc09245975f2c906114b1b687430a8937add69
arashmad <arashmad.dev@gmail.com>
2025-01-17 22:32:36 +0100

    validate email and passwords fields

--- user_management_py/schemas/users.py
+++ user_management_py/schemas/users.py
@@ -4,17 +4,39 @@ Fastapi Poetry Boilerplate.
 A boilerplate for fastapi python project supported by poetry.
 """
 
-from pydantic import BaseModel, Field
+from typing import Any
+from pydantic import BaseModel, Field, field_validator
+
 from user_management_py.models.users import UserCreate, UserRead
+
+from user_management_py.utils.field_validator import validate_email, validate_password
 
 
 class CreateUserRequest(UserCreate):
     """Docstring."""
+
+    @field_validator("email")
+    @classmethod
+    def check_field_email(cls, email: str) -> Any:
+        """Check the given email."""
+        if not validate_email(email):
+            raise ValueError("Invalid email address.")
+        return email
+
+    @field_validator("password")
+    @classmethod
+    def check_field_password(cls, password: str) -> Any:
+        """Check the given password."""
+        is_valid, message = validate_password(password)
+        if not is_valid:
+            raise ValueError(message)
+        return password
+
     model_config = {
         "json_schema_extra": {
             "title": "Message",
             "example": {
-                "email": "john@doe@user.com",
+                "email": "john.doe@user.domain",
                 "password": "xxxxxxxxxxxx"
             }
         }
