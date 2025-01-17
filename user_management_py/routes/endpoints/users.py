55dfb08a4f1af5bb3bd759b7af31204e7b1fee5b
arashmad <arashmad.dev@gmail.com>
2025-01-17 22:31:52 +0100

    used hashed password

--- user_management_py/routes/endpoints/users.py
+++ user_management_py/routes/endpoints/users.py
@@ -4,15 +4,21 @@ Fastapi Poetry Boilerplate.
 A boilerplate for fastapi python project supported by poetry.
 """
 
-import user_management_py.schemas.general as general_schema
-import user_management_py.schemas.users as users_schema
+from sqlmodel import Session
+
+
 from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
-from sqlmodel import Session
+
 from user_management_py.db.connection import get_session
 from user_management_py.models.users import Users
+from user_management_py.utils.auth import pwd_context
 
-router = APIRouter()
+import user_management_py.schemas.general as general_schema
+import user_management_py.schemas.users as users_schema
+
+
+router = APIRouter(tags=["Users"], prefix="/users")
 
 
 @router.post('/',
@@ -23,11 +29,12 @@ router = APIRouter()
                  404: {"model": general_schema.Message},
                  500: {"model": general_schema.Message}})
 def create_user(
-        user: users_schema.CreateUserRequest,
+        payload: users_schema.CreateUserRequest,
         session: Session = Depends(get_session)):
     """Docstring."""
+    password_hash = pwd_context.hash(payload.password)
+    new_user = Users(email=payload.email, password=password_hash)
 
-    new_user = Users(email=user.email, password=user.password)
     session.add(new_user)
     session.commit()
     session.refresh(new_user)
