20eb178fdd0607eb5f4b2aa38b3d027c6ae3f553
arashmad <arashmad.dev@gmail.com>
2025-01-17 22:30:11 +0100

    cleaned code by defining prefix and tag in router itself

--- user_management_py/routes/api.py
+++ user_management_py/routes/api.py
@@ -8,9 +8,10 @@ from fastapi import APIRouter
 
 import user_management_py.routes.endpoints.home as home_route
 import user_management_py.routes.endpoints.users as users_route
+import user_management_py.routes.endpoints.auth as auth_route
 
 router = APIRouter()
 
-router.include_router(home_route.router, tags=["Home"])
-router.include_router(users_route.router, tags=["Users"],
-                      prefix="/users")
+router.include_router(home_route.router)
+router.include_router(users_route.router)
+router.include_router(auth_route.router)
