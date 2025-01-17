958d7aff2688640f43b3151ad999d8744bcc9799
arashmad <arashmad.dev@gmail.com>
2025-01-17 23:10:14 +0100

    developed /auth/login endpoint

---
+++ user_management_py/routes/endpoints/auth.py
@@ -0,0 +1,68 @@
+"""
+Fastapi Poetry Boilerplate.
+
+A boilerplate for fastapi python project supported by poetry.
+"""
+
+from sqlmodel import Session, select
+from datetime import timedelta
+
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import JSONResponse
+
+from user_management_py.models.users import Users
+from user_management_py.db.connection import get_session
+from user_management_py.utils.auth import pwd_context, create_access_token
+
+import user_management_py.schemas.general as general_schema
+import user_management_py.schemas.auth as auth_schema
+
+router = APIRouter(tags=["Authentication"], prefix="/auth")
+
+
+@router.post("/login",
+             summary="Login user",
+             responses={
+                 200: {"model": general_schema.Message},
+                 401: {"model": general_schema.Message},
+                 403: {"model": general_schema.Message},
+                 404: {"model": general_schema.Message},
+                 500: {"model": general_schema.Message}
+             },
+             response_model=auth_schema.LoginResponse)
+def login(
+    payload: auth_schema.LoginRequest,
+    session: Session = Depends(get_session)
+):
+    """Docstring."""
+
+    statement = select(Users).where(Users.email == payload.email)
+    user = session.exec(statement).first()
+
+    if not user:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={
+                "msg": f"User not found."
+            })
+
+    if not pwd_context.verify(payload.password, user.password):
+        return JSONResponse(
+            status_code=status.HTTP_403_FORBIDDEN,
+            content={
+                "msg": f"Invalid credentials."
+            })
+
+    access_token_expires = timedelta(minutes=30)
+    access_token = create_access_token(
+        email=user.email,
+        user_id=user.user_id,
+        salt=user.salt_jwt,
+        expires_delta=access_token_expires)
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "access_token": access_token,
+            "refresh_token": "refresh_token"
+        })
