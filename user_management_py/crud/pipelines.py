e6e0d1d282bed5394b2e08bf6f7b6b5317b524fc
arashmad <arashmad.dev@gmail.com>
2025-01-24 22:34:42 +0100

    crud method for fetching pipelines of specific type

--- user_management_py/crud/pipelines.py
+++ user_management_py/crud/pipelines.py
@@ -1,10 +1,33 @@
 """Docstring."""
 
-from sqlmodel import Session
+from sqlmodel import Session, select
 
 from fastapi import HTTPException, status
 
 from user_management_py.models.pipelines import Pipelines, PipelineRead
+
+
+def fetch_all_pipelines(
+        pipeline_type: str,
+        user_id: str,
+        session: Session) -> list[PipelineRead]:
+    """Fetch all pipelines with the given type and user_id."""
+    try:
+        query = select(Pipelines).where(
+            Pipelines.pipeline_type == pipeline_type,
+            Pipelines.user_id == user_id)
+        results = session.exec(query)
+
+        return [pipeline.to_dict() for pipeline in results]
+
+    except ValueError as ve:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(ve)) from ve
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)) from e
 
 
 def create_pipeline(
