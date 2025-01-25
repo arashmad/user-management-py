6a1d09782cfa9bfe178cfe891443d8a08f13930c
arashmad <arashmad.dev@gmail.com>
2025-01-25 22:43:29 +0100

    fixed bugs + new crud func for fetching pipeline with id

--- user_management_py/crud/pipelines.py
+++ user_management_py/crud/pipelines.py
@@ -4,30 +4,80 @@ from sqlmodel import Session, select
 
 from fastapi import HTTPException, status
 
+from user_management_py.enum.pipeline import PipelineType
 from user_management_py.models.pipelines import Pipelines, PipelineRead
 
+PIPELINE_TYPES = [p.value for p in PipelineType]
 
-def fetch_all_pipelines(
-        pipeline_type: str,
-        user_id: str,
-        session: Session) -> list[PipelineRead]:
-    """Fetch all pipelines with the given type and user_id."""
+
+def fetch_pipeline_by_type(
+        pipeline_type: str, user_id: str, session: Session) -> list[PipelineRead]:
+    """
+    Fetch pipelines by type.
+
+    Parameters
+    ----------
+    pipeline_type : str
+        Type of the pipeline from list ['sentinel', 'misac', 'xregnet']
+    user_id : str
+        Unique identifier for the user.
+    session : Session
+        SQLModel Session object.
+
+    Returns
+    -------
+    list[PipelineRead]
+        List of pipelines.
+    """
     try:
-        query = select(Pipelines).where(
-            Pipelines.pipeline_type == pipeline_type,
-            Pipelines.user_id == user_id)
-        results = session.exec(query)
+        if pipeline_type not in PIPELINE_TYPES:
+            raise ValueError(
+                f"Type ({pipeline_type}) not supported. Choose from [{PIPELINE_TYPES}].")
 
+        query = \
+            select(Pipelines).\
+            where(Pipelines.pipeline_type == pipeline_type).\
+            where(Pipelines.user_id == user_id)
+
+        results = session.exec(query)
         return [pipeline.to_dict() for pipeline in results]
 
-    except ValueError as ve:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=str(ve)) from ve
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=str(e)) from e
+
+
+def fetch_pipeline_by_id(
+        pipeline_id: str, user_id: str, session: Session) -> PipelineRead:
+    """Fetch a pipeline by ID.
+
+    Parameters
+    ----------
+    pipeline_id : str
+        Unique identifier for the pipeline.
+    user_id : str
+        Unique identifier for the user.
+    session : Session
+        SQLModel Session object.
+
+    Returns
+    -------
+    PipelineRead
+        Pipeline details.
+    """
+    statement = \
+        select(Pipelines).\
+        where(Pipelines.pipeline_id == pipeline_id).\
+        where(Pipelines.user_id == user_id)
+    pipeline = session.exec(statement).first()
+
+    if not pipeline:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Pipeline ({pipeline_id}) not found.")
+
+    return pipeline.to_dict()
 
 
 def create_pipeline(
