67609fb374c632d2f89aab54b84e5c9d97841ddf
arashmad <arashmad.dev@gmail.com>
2025-01-27 14:36:10 +0100

    init database only in production/deployment mode

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from uvicorn import run
 
+from user_management_py.db.connection import init_db
+
 if __name__ == "__main__":
+    init_db()
     run(
         "user_management_py.create_app:app",
         host="0.0.0.0",
